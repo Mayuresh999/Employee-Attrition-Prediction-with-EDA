591e71896d79b818d4867f8d04232dc0f528cb48
Mayuresh N. Madiwale <95702726+Mayuresh999@users.noreply.github.com>
2023-01-02 11:39:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import pandas as pd
 from flask import Flask,request,render_template
 import pickle
-
+import base64
+import streamlit as st
+import streamlit.components.v1 as components
+st.title('HTML test')
+HtmlFile = open('employee.html', 'r')
+raw_html = HtmlFile.read().encode("utf-8")
+raw_html = base64.b64encode(raw_html).decode()
+components.iframe(f"data:text/html;base64,{raw_html}", height=400)
 
 app = Flask (__name__)
 model = pickle.load (open ('model.pkl','rb'))
